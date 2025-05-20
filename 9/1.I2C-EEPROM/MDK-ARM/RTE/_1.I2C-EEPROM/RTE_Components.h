8663ab9c1c1ecb7ef9a0a7f4ee0ff26c6f3026b0
Danial Mobini <danialmobinidh81@gmail.com>
2025-05-20 21:17:23 +0330

    added sesion 9 i2c

---
+++ 9/1.I2C-EEPROM/MDK-ARM/RTE/_1.I2C-EEPROM/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '1.I2C-EEPROM' 
+ * Target:  '1.I2C-EEPROM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ 9/3.I2C-Interupt/MDK-ARM/RTE/_3.I2C-Interupt/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '3.I2C-Interupt' 
+ * Target:  '3.I2C-Interupt' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
